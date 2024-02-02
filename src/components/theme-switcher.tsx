beebef97f712324efd35926578e4c2a62e22659a
UpalBarua <upalbarua52@gmail.com>
2024-02-02 09:10:54 +0600

    first commit

---
+++ src/components/theme-switcher.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Switch } from '@nextui-org/react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+export const ThemeSwitcher = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Switch
+      defaultSelected
+      size="lg"
+      color="primary"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <Moon className={className} size={16} />
+        ) : (
+          <Sun className={className} size={16} />
+        )
+      }
+    />
+  );
+};
