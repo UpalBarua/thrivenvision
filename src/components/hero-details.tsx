71ba534e578ecfd256e3021663cb624075608899
UpalBarua <upalbarua52@gmail.com>
2024-02-20 20:54:33 +0600

    fix: refactor the hero section

--- src/components/hero-details.tsx
+++ src/components/hero-details.tsx
@@ -6,27 +6,13 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-const ServiceCategoryIds = services.map((service) => service.id);
-
 export function HeroDetails() {
-  const [scrollX, setScrollX] = useState(80);
-
-  const [focusedService, setFocusedService] = useState<
-    (typeof ServiceCategoryIds)[number]
-  >(ServiceCategoryIds[2]);
+  const [scrollX, setScrollX] = useState(40);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setFocusedService((prevService) => {
-        const currentServiceIdIndex = ServiceCategoryIds.indexOf(prevService);
-        const nextServiceIdIndex =
-          currentServiceIdIndex === 5 ? 0 : currentServiceIdIndex + 1;
-        return ServiceCategoryIds[nextServiceIdIndex];
-      });
-
       setScrollX((prev) => {
         if (prev >= services.length * 40) {
-          setFocusedService(ServiceCategoryIds[2]);
           return 0;
         } else {
           return prev + 40;
@@ -38,34 +24,28 @@ export function HeroDetails() {
   }, []);
 
   return (
-    <div className="flex w-full items-center gap-8 overflow-hidden rounded-2xl">
-      {[...services, ...services, ...services].map(
-        ({ id, categoryImage, category }) => (
-          <motion.div
-            className={cn(
-              "relative h-full min-w-[38rem] transition-all duration-[0.8] ease-in-out",
-            )}
-            key={id}
-            animate={{ x: `-${scrollX}rem` }}
-            transition={{
-              type: "spring",
-              duration: 0.5,
-              stiffness: 40,
-            }}
-          >
-            <Image
-              className="rounded-2xl object-cover object-center shadow-md"
-              alt="NextUI hero Image with delay"
-              src={'/test.png'}
-              // src={categoryImage}
-              fill
-            />
-            <span className="absolute inset-0  text-2xl font-bold text-red-500">
-              {category}
-            </span>
-          </motion.div>
-        ),
-      )}
+    <div className="flex h-full w-full items-center gap-8 overflow-hidden rounded-2xl">
+      {[...services, ...services, ...services].map(({ id, categoryImage }) => (
+        <motion.div
+          className={cn(
+            "relative h-full min-w-[38rem] transition-all duration-[0.8] ease-in-out",
+          )}
+          key={id}
+          animate={{ x: `-${scrollX}rem` }}
+          transition={{
+            type: "spring",
+            duration: 0.5,
+            stiffness: 40,
+          }}
+        >
+          <Image
+            className="object-fit rounded-2xl object-bottom"
+            alt="NextUI hero Image with delay"
+            src={categoryImage}
+            fill
+          />
+        </motion.div>
+      ))}
     </div>
   );
 }
