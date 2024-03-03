b22b97959102c9173bea029ae76e5b0d0d2ca2ba
UpalBarua <upalbarua52@gmail.com>
2024-03-03 22:04:26 +0600

    uncomment components from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,25 +13,25 @@ import { ConsultantBookingModal } from "@/components/consultant-booking-modal";
 export default function HomePage() {
   return (
     <main className="container relative z-20 mx-auto max-w-7xl space-y-[min(10rem,25dvw)] px-2 sm:px-4">
-      {/* <ConsultantBookingForm /> */}
-      {/* <ConsultantBookingModal /> */}
+      <ConsultantBookingForm />
+      <ConsultantBookingModal />
       <Hero />
-      {/* <Services /> */}
-      {/* <PricingPackages /> */}
-      {/* <WhyChooseUs /> */}
-      {/* <TrustedCompanies /> */}
-      {/* <Portfolios /> */}
-      {/* <Testimonials /> */}
-      {/* <Newsletter /> */}
-      {/* <Image */}
-      {/*   className="object-fit absolute -top-[22dvw] right-0 h-[15rem] opacity-30 lg:-top-[4dvw] lg:h-[30rem]" */}
-      {/*   src={"/images/hero-bg-logo.webp"} */}
-      {/*   alt="hero bg image" */}
-      {/*   height="600" */}
-      {/*   width="600" */}
-      {/*   quality={95} */}
-      {/*   priority */}
-      {/* /> */}
+      <Services />
+      <PricingPackages />
+      <WhyChooseUs />
+      <TrustedCompanies />
+      <Portfolios />
+      <Testimonials />
+      <Newsletter />
+      <Image
+        className="object-fit absolute -top-[22dvw] right-0 h-[15rem] opacity-30 lg:-top-[4dvw] lg:h-[30rem]"
+        src={"/images/hero-bg-logo.webp"}
+        alt="hero bg image"
+        height="600"
+        width="600"
+        quality={95}
+        priority
+      />
     </main>
   );
 }
