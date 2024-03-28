2b4aea44f0ebbd881986306dc8e2a75bcee65b78
UpalBarua <upalbarua52@gmail.com>
2024-03-28 10:17:19 +0600

    remake the trusted-company slider

--- src/components/trusted-companies-slider.tsx
+++ src/components/trusted-companies-slider.tsx
@@ -1,24 +1,47 @@
-"use client";
-
-import Marquee from "react-fast-marquee";
 import { trustedCompanines } from "@/config";
+import { ScrollShadow } from "@nextui-org/scroll-shadow";
 import Image from "next/image";
 
 export default function TrustedCompaniesSlider() {
   return (
-    <Marquee className="lg:!hidden">
-      {trustedCompanines?.map((company) => (
-        <div key={company} className="mx-6">
-          <Image
-            src={company}
-            alt="trusted company"
-            height={80}
-            width={120}
-            className="object-fit object-center"
-            quality={95}
-          />
+    <div>
+      <ScrollShadow
+        hideScrollBar
+        visibility="both"
+        orientation="horizontal"
+        size={25}
+      >
+        <div className="relative flex overflow-hidden">
+          <div className="animate-marquee flex items-center justify-center gap-x-4 whitespace-nowrap">
+            {trustedCompanines?.map((company) => (
+              <div key={company} className="mx-6">
+                <Image
+                  src={company}
+                  alt="trusted company"
+                  height={100}
+                  width={120}
+                  className="object-fit min-w-[10rem] object-center"
+                  quality={95}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="animate-marquee2 absolute top-0 flex items-center justify-center gap-x-4 whitespace-nowrap">
+            {trustedCompanines?.map((company, i) => (
+              <div key={company + i} className="mx-6">
+                <Image
+                  src={company}
+                  alt="trusted company"
+                  height={100}
+                  width={120}
+                  className="object-fit min-w-[10rem] object-center"
+                  quality={95}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-    </Marquee>
+      </ScrollShadow>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,25 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/components/(button|avatar|card|dropdown|input|modal|navbar|radio|scroll-shadow|select|switch|table|tabs).js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: "marquee 20s linear infinite",
+        marquee2: "marquee2 20s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
   },
   darkMode: "class",
   plugins: [
