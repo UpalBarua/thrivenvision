54cfe3365e370da1dfa2840828d11f45de36a361
UpalBarua <upalbarua52@gmail.com>
2024-04-08 17:42:34 +0600

    changes

--- src/components/portfolio-cards.tsx
+++ src/components/portfolio-cards.tsx
@@ -6,7 +6,7 @@ export async function PortfolioCards() {
 
   return (
     <div className="grid grid-cols-1 gap-4 px-0 pt-4 md:grid-cols-2 lg:grid-cols-3 lg:px-4">
-      {portfolios.map(({ id, instagramURL }) => (
+      {portfolios.slice(0, 3).map(({ id, instagramURL }) => (
         <PortfolioPost key={id} url={instagramURL} />
       ))}
     </div>
